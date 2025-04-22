607c9f02745d34b8129d46944ae9f3e7545546f1
Beyazit Yalcinkaya <beyazit@wifi-10-42-206-55.wifi.berkeley.edu>
2025-04-22 11:42:40 -0700

    Add working copy

--- dfa_gym/__init__.py
+++ dfa_gym/__init__.py
@@ -1,14 +1,10 @@
 from dfa_gym.dfa_env import *
 from dfa_gym.dfa_wrapper import *
 from dfa_gym.dfa_bisim_env import *
-from dfa_gym.dfa_bisim2_env import *
-
-from dfa_gym.word_env import *
+from dfa_gym.dfa_bisim_prob_env import *
 
 from dfa_samplers import RADSampler
 from gymnasium.envs.registration import register
-
-from rad_embeddings import Encoder
 
 register(
     id='DFAEnv-v0',
@@ -27,14 +23,7 @@ register(
     entry_point='dfa_gym.dfa_bisim_env:DFABisimEnv'
 )
 
-
 register(
-    id='DFABisimEnv-v2',
-    entry_point='dfa_gym.dfa_bisim2_env:DFABisim2Env'
-)
-
-register(
-    id='WordEnv-v1',
-    entry_point='dfa_gym.word_env:WordEnv',
-    kwargs = {"encoder": Encoder(load_file="DFABisimEnv-v1-encoder")}
+    id='DFABisimProbEnv-v1',
+    entry_point='dfa_gym.dfa_bisim_prob_env:DFABisimProbEnv'
 )
--- test.py
+++ test.py
@@ -1,46 +1,47 @@
 import gymnasium as gym
 
-from dfa_gym import DFAEnv, DFAWrapper, WordEnv
+from dfa_gym import DFAEnv, DFAWrapper
 
-from rad_embeddings import Encoder
+from stable_baselines3.common.env_checker import check_env
 
 if __name__ == "__main__":
-    # Test DFAEnv
-    dfa_env = gym.make("DFAEnv-v1")
-    obs, info = dfa_env.reset()
+    # # Test DFAEnv
+    # dfa_env = gym.make("DFAEnv-v1")
+    # obs, info = dfa_env.reset()
+    # for _ in range(1000):
+    #     action = dfa_env.action_space.sample()  # Random action
+    #     obs, reward, done, truncated, info = dfa_env.step(action)
+    #     if done:
+    #         break
+    # dfa_env.close()
+    # # Test DFAWrapper
+    # env_cls = "CartPole-v1"
+    # wrapped_env = DFAWrapper(env_cls)
+    # observation, info = wrapped_env.reset()
+    # done = False
+    # i = 0
+    # while not done:
+    #     i += 1
+    #     action = wrapped_env.action_space.sample()
+    #     observation, reward, terminated, truncated, info = wrapped_env.step(action)
+    #     done = terminated or truncated
+    # wrapped_env.close()
+    # # Test DFAEnv
+    # dfa_env = gym.make("DFABisimEnv-v1")
+    # obs, info = dfa_env.reset()
+    # for _ in range(1000):
+    #     action = dfa_env.action_space.sample()  # Random action
+    #     obs, reward, done, truncated, info = dfa_env.step(action)
+    #     if done:
+    #         break
+    # dfa_env.close()
+    # Test DFABisimProbEnv
+    env = gym.make("DFABisimProbEnv-v1")
+    check_env(env)
+    obs, info = env.reset()
     for _ in range(1000):
-        action = dfa_env.action_space.sample()  # Random action
-        obs, reward, done, truncated, info = dfa_env.step(action)
+        action = env.action_space.sample()  # Random action
+        obs, reward, done, truncated, info = env.step(action)
         if done:
             break
-    dfa_env.close()
-    # Test DFAWrapper
-    env_cls = "CartPole-v1"
-    wrapped_env = DFAWrapper(env_cls)
-    observation, info = wrapped_env.reset()
-    done = False
-    i = 0
-    while not done:
-        i += 1
-        action = wrapped_env.action_space.sample()
-        observation, reward, terminated, truncated, info = wrapped_env.step(action)
-        done = terminated or truncated
-    wrapped_env.close()
-    # Test DFAEnv
-    dfa_env = gym.make("DFABisimEnv-v2")
-    obs, info = dfa_env.reset()
-    for _ in range(1000):
-        action = dfa_env.action_space.sample()  # Random action
-        obs, reward, done, truncated, info = dfa_env.step(action)
-        if done:
-            break
-    dfa_env.close()
-    # Test WordEnv
-    word_env = gym.make("WordEnv-v1")
-    obs, info = word_env.reset()
-    for _ in range(1000):
-        action = word_env.action_space.sample()  # Random action
-        obs, reward, done, truncated, info = word_env.step(action)
-        if done:
-            break
-    word_env.close()
+    env.close()
