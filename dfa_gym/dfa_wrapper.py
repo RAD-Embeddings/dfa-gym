0deb33a7b1affda7c43b50bc8fa13d98011c2445
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-09-08 21:47:02 -0700

    Reward based on overall reward

--- dfa_gym/dfa_wrapper.py
+++ dfa_gym/dfa_wrapper.py
@@ -134,9 +134,16 @@ class DFAWrapper(MultiAgentEnv):
             agent: jax.lax.cond(
                 state.dfa_dones[agent],
                 lambda _: 0.0,
-                lambda _: dfas[agent].reward() * self.self_reward_ratio,
+                # lambda _: dfas[agent].reward() * self.self_reward_ratio,
+                lambda _: dfas[agent].reward(),
                 operand=None
             )
+            for agent in self.agents
+        }
+
+        overall_dfa_reward = jnp.sum(jnp.array([dfa_rewards[agent] for agent in self.agents])) / self.num_agents
+        rewards = {
+            agent: env_rewards[agent] + overall_dfa_reward
             for agent in self.agents
         }
 
@@ -152,16 +159,16 @@ class DFAWrapper(MultiAgentEnv):
         _dones = jnp.array([dones[agent] for agent in self.agents])
         dones.update({"__all__": jnp.all(_dones)})
 
-        overall_dfa_reward = jnp.sum(jnp.array([dfas[agent].reward() for agent in self.agents])) / self.num_agents
-        rewards = {
-            agent: jax.lax.cond(
-                dones["__all__"],
-                lambda _: env_rewards[agent] + dfa_rewards[agent] + overall_dfa_reward,
-                lambda _: env_rewards[agent] + dfa_rewards[agent],
-                operand=None
-            )
-            for agent in self.agents
-        }
+        # overall_dfa_reward = jnp.sum(jnp.array([dfas[agent].reward() for agent in self.agents])) / self.num_agents
+        # rewards = {
+        #     agent: jax.lax.cond(
+        #         dones["__all__"],
+        #         lambda _: env_rewards[agent] + dfa_rewards[agent] + overall_dfa_reward,
+        #         lambda _: env_rewards[agent] + dfa_rewards[agent],
+        #         operand=None
+        #     )
+        #     for agent in self.agents
+        # }
 
         infos = {}
 
